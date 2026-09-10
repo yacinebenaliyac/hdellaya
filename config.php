8d313b5ca418dd1fba41d6a24da05c713e0a1204
Yacine BENALI <164084889+yacinebenaliyac@users.noreply.github.com>
2026-09-10 11:27:08 +0100

    Update config.php

--- config.php
+++ config.php
@@ -5,9 +5,11 @@
 if (session_status() === PHP_SESSION_NONE) session_start();
 
 define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('DB_PORT', getenv('DB_PORT') ?: '3306');
 define('DB_NAME', getenv('DB_NAME') ?: 'hdellaya');
 define('DB_USER', getenv('DB_USER') ?: 'root');
 define('DB_PASS', getenv('DB_PASS') ?: '');
+
 define('ADMIN_USERNAME', getenv('ADMIN_USERNAME') ?: 'admin');
 define('ADMIN_PASSWORD', getenv('ADMIN_PASSWORD') ?: 'dellaya2024');
 
